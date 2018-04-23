89b8c7ff254c1f2ead3b819b60bd62a47ba75d32
詹叶 <zhany@HeisenbergMAC.local>
2018-04-24 01:00:14 +0800

    add

---
+++ src/test/java/com/piggymetrics/statistics/client/ExchangeRatesClientTest.java
@@ -0,0 +1,37 @@
+package com.piggymetrics.statistics.client;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.piggymetrics.statistics.StatisticsApplication;
+import com.piggymetrics.statistics.domain.Currency;
+import com.piggymetrics.statistics.domain.ExchangeRatesContainer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = StatisticsApplication.class)
+public class ExchangeRatesClientTest {
+
+	@Autowired
+	private ExchangeRatesClient client;
+
+	@Test
+	public void shouldRetrieveExchangeRates() {
+
+		ExchangeRatesContainer container = client.getRates(Currency.getBase());
+
+		assertEquals(container.getDate(), LocalDate.now());
+		assertEquals(container.getBase(), Currency.getBase());
+
+		assertNotNull(container.getRates().get(Currency.EUR.name()));
+		assertNotNull(container.getRates().get(Currency.RUB.name()));
+	}
+
+}
